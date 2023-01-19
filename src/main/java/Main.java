79ea5f92e730c4d181ea01d8e7f8ee6c550246a2
kholodovaMB <holodova.maryna@gmail.com>
2023-01-19 21:34:02 +0200

    start

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("фигура");
+        Shape shape = new Shape();
+        shape.test();
+    }
+}
---
+++ src/main/java/Shape.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+public class Shape {
+    public void test(){
+        System.out.println("тест");
+    }
+}
