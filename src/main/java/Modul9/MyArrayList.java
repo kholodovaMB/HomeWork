a76c81bbc422d40b99bbebc70f241447bed7d9d9
kholodovaMB <holodova.maryna@gmail.com>
2023-02-05 16:41:26 +0200

    new Class MaArrayList

--- src/main/java/Modul9/MyArrayList.java
+++ src/main/java/Modul9/MyArrayList.java
@@ -8,14 +8,22 @@ public class MyArrayList <E> extends AbstractList<E> implements List<E> {
     private final static int CAPACITY = 10;
     private int size = 0;
     private Object[] DEFAULT_DATA = {};
+    transient Object[] elementData;
 
 //    public MyArrayList() {
 //        E[] DEFAULT_DATA;
 //        DEFAULT_DATA = new E[10];
 //    }
 
-    public MyArrayList(int size) {
-        this.size = size;
+    public MyArrayList(int initSize) {
+        if (initSize > 0) {
+            this.elementData = new Object[initSize];
+        } else if (initSize == 0) {
+            this.elementData = new Object[]{};
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: "+
+                    initSize);
+        }
     }
 
     public MyArrayList(List<? extends E> data) {
