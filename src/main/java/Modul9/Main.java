6fcbe12b54157e4f7659b0436c6e26d9c2df77a9
kholodovaMB <holodova.maryna@gmail.com>
2023-02-05 16:04:54 +0200

    new Modul 9

---
+++ src/main/java/Modul9/Main.java
@@ -0,0 +1,12 @@
+package Modul9;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> test = new ArrayList<>();
+        test.add("test first");
+        System.out.println(test);
+    }
+}
---
+++ src/main/java/Modul9/MyArrayList.java
@@ -0,0 +1,66 @@
+package Modul9;
+
+import java.util.AbstractList;
+import java.util.Collection;
+import java.util.List;
+
+public class MyArrayList <E> extends AbstractList<E> implements List<E> {
+    private final static int CAPACITY = 10;
+    private int size = 0;
+    private Object[] DEFAULT_DATA = {};
+
+//    public MyArrayList() {
+//        E[] DEFAULT_DATA;
+//        DEFAULT_DATA = new E[10];
+//    }
+
+    public MyArrayList(int size) {
+        this.size = size;
+    }
+
+    public MyArrayList(List<? extends E> data) {
+        this.size = data.size();
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    private boolean add() {
+        return add(null);
+    }
+
+//    private boolean add(E e) {
+//
+//    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+//    private void add(E e, Object[] elementData, int s) {
+//        if (s == elementData.length)
+//            elementData = grow();
+//        elementData[s] = e;
+//        size = s + 1;
+//    }
+
+//    private Object[] grow() {
+//        return grow(size + 1);
+//    }
+
+//    private Object[] grow(int minCapacity) {
+//        int oldCapacity = elementData.length;
+//        if (oldCapacity > 0 || elementData != DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
+//            int newCapacity = ArraysSupport.newLength(oldCapacity,
+//                    minCapacity - oldCapacity, /* minimum growth */
+//                    oldCapacity >> 1           /* preferred growth */);
+//            return elementData = Arrays.copyOf(elementData, newCapacity);
+//        } else {
+//            return elementData = new Object[Math.max(DEFAULT_CAPACITY, minCapacity)];
+//        }
+//    }
+}
