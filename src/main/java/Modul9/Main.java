95c7ea282bba3f266274d1f47e9a09be63114f76
kholodovaMB <holodova.maryna@gmail.com>
2023-02-06 21:51:56 +0200

    add methods

--- src/main/java/Modul9/Main.java
+++ src/main/java/Modul9/Main.java
@@ -1,12 +1,21 @@
 package Modul9;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<String> test = new ArrayList<>();
         test.add("test first");
+        test.add("test second");
+
         System.out.println(test);
+        List<String> myTest = new MyArrayList<String>();
+        myTest.add("hello");
+        myTest.add("fgfhfgh");
+        System.out.println("myTest - " + myTest);
+        System.out.println(myTest.size());
+       System.out.println(myTest.get(1));
     }
 }
--- src/main/java/Modul9/MyArrayList.java
+++ src/main/java/Modul9/MyArrayList.java
@@ -1,74 +1,100 @@
 package Modul9;
 
-import java.util.AbstractList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+import jdk.internal.util.ArraysSupport;
 
-public class MyArrayList <E> extends AbstractList<E> implements List<E> {
+public class MyArrayList <E>  extends AbstractList<E> implements List<E>{
     private final static int CAPACITY = 10;
-    private int size = 0;
-    private Object[] DEFAULT_DATA = {};
+    private int size;
+    private final Object[] DEFAULT_DATA = {};
     transient Object[] elementData;
-
-//    public MyArrayList() {
-//        E[] DEFAULT_DATA;
-//        DEFAULT_DATA = new E[10];
-//    }
+    private int modCount = 0;
+    public MyArrayList() {
+        this.elementData = DEFAULT_DATA;
+    }
 
     public MyArrayList(int initSize) {
         if (initSize > 0) {
             this.elementData = new Object[initSize];
         } else if (initSize == 0) {
-            this.elementData = new Object[]{};
+            this.elementData = DEFAULT_DATA;
         } else {
             throw new IllegalArgumentException("Illegal Capacity: "+
                     initSize);
         }
     }
 
-    public MyArrayList(List<? extends E> data) {
+    public MyArrayList(Collection <? extends E> data) {
         this.size = data.size();
-
+        Object[] a = data.toArray();
+        if (size != 0) {
+            if (data.getClass() == MyArrayList.class) {
+                elementData = a;
+            } else {
+                elementData = Arrays.copyOf(a, size, Object[].class);
+            }
+        } else {
+            elementData = DEFAULT_DATA;
+        }
     }
 
-    @Override
+    public Object[] toArray() {
+        return Arrays.copyOf(elementData, size);
+    }
+
+    public <T> T[] toArray(T[] a) {
+        if (a.length < size)
+            // Make a new array of a's runtime type, but my contents:
+            return (T[]) Arrays.copyOf(elementData, size, a.getClass());
+        System.arraycopy(elementData, 0, a, 0, size);
+        if (a.length > size)
+            a[size] = null;
+        return a;
+    }
+
+
     public int size() {
-        return 0;
+        return size;
     }
 
-    private boolean add() {
-        return add(null);
+    public boolean add(E e) {
+        modCount++;
+//        System.out.println("e = " + e);
+//        System.out.println("modCount = " + modCount);
+        add(e, elementData, size);
+        return true;
     }
-
-//    private boolean add(E e) {
-//
-//    }
 
     @Override
     public E get(int index) {
-        return null;
+        System.out.println(size);
+        if (index < size) {
+            return (E) elementData[index];}
+        else {
+            System.out.println("Don't exist element");
+        }
+        return null;}
+
+    private void add(E e, Object[] elementData, int s) {
+        if (s == elementData.length)
+            elementData = grow();
+        elementData[s] = e;
+        size = s + 1;
     }
 
-//    private void add(E e, Object[] elementData, int s) {
-//        if (s == elementData.length)
-//            elementData = grow();
-//        elementData[s] = e;
-//        size = s + 1;
-//    }
+    private Object[] grow() {
+        return grow(size + 1);
+    }
 
-//    private Object[] grow() {
-//        return grow(size + 1);
-//    }
-
-//    private Object[] grow(int minCapacity) {
-//        int oldCapacity = elementData.length;
-//        if (oldCapacity > 0 || elementData != DEFAULTCAPACITY_EMPTY_ELEMENTDATA) {
-//            int newCapacity = ArraysSupport.newLength(oldCapacity,
-//                    minCapacity - oldCapacity, /* minimum growth */
-//                    oldCapacity >> 1           /* preferred growth */);
-//            return elementData = Arrays.copyOf(elementData, newCapacity);
-//        } else {
-//            return elementData = new Object[Math.max(DEFAULT_CAPACITY, minCapacity)];
-//        }
-//    }
+    private Object[] grow(int minCapacity) {
+        int oldCapacity = elementData.length;
+        if (oldCapacity > 0 || elementData != DEFAULT_DATA) {
+            int newCapacity = ArraysSupport.newLength(oldCapacity,
+                    minCapacity - oldCapacity, /* minimum growth */
+                    oldCapacity >> 1           /* preferred growth */);
+            return elementData = Arrays.copyOf(elementData, newCapacity);
+        } else {
+            return elementData = new Object[Math.max(CAPACITY, minCapacity)];
+        }
+    }
 }
